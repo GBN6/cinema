367b486a2f60a01db7101cf21a3333d979821579
GBN6 <czesiox8@gmail.com>
2022-11-26 13:48:42 +0100

    Add initial seat grid layout

--- src/app/pages/seats/seats.component.ts
+++ src/app/pages/seats/seats.component.ts
@@ -9,6 +9,24 @@ export class SeatsComponent implements OnInit {
 
   constructor() { }
 
+  rows: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'];
+  cols: number[]  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+  reserved: string[] = ['A2', 'A3', 'F5', 'F1', 'F2','F6', 'F7', 'F8', 'H1', 'H2', 'H3', 'H4'];
+  selected: string[] = [];
+
+  getStatus(seatPos: string){
+    if(this.reserved.indexOf(seatPos) !== -1) {
+        return 'reserved';
+    } else if (this.selected.indexOf(seatPos) !== -1) {
+        return 'selected';
+    } return 'freeSeat'
+}
+
+  getAlphabet(){
+    
+  }
+
   ngOnInit(): void {
   }
 
